eee36ac32923be32eb08ef831b445caa4c1afd52
Boj Yoann <yoann.boj@safrangroup.com>
2019-01-19 23:45:25 +0100

    added evaluation of game and many tests.

--- src/greed.py
+++ src/greed.py
@@ -9,7 +9,7 @@ class Greed():
             min_dice_value: int = 1,
             max_dice_value: int = 6
     ):
-        self.score = 0
+        self.score_value = 0
         self.number_of_dices = number_of_dices
         self.min_dice_value = min_dice_value
         self.max_dice_value = max_dice_value
@@ -27,15 +27,93 @@ class Greed():
         self.values[str(value)] += 1
 
     def _fill_values(self, dice_values: List[int]):
+        self._reset_values()
+
         for v in dice_values:
             if v < self.min_dice_value or v > self.max_dice_value:
                 raise ValueError("Dice can not take this value.")
 
             self._increment_value(v)
 
+    def _is_single_one(self):
+        return self.values["1"] == 1
+
+    def _is_single_five(self):
+        return self.values["5"] == 1
+
+    def _is_triple_with_value(self, dice_value: int):
+        return self.values[str(dice_value)] == 3
+
+    def _is_triple_with_value(self, dice_value: int):
+        for i in range(self.min_dice_value, self.max_dice_value + 1):
+            if self.values[str(i)] == 3:
+                return True
+
+        return False
+
+    def _is_a_kind(self, number_of_values: int):
+        for i in range(self.min_dice_value, self.max_dice_value + 1):
+            if self.values[str(i)] == number_of_values:
+                return True
+
+        return False
+
+    def _is_three_pairs(self):
+        number_of_pairs = 0
+
+        for key, value in self.values.items():
+            if value == 2:
+                number_of_pairs += 1
+
+        return number_of_pairs == 3
+
+    def _is_straigth(self):
+        for i in range(self.min_dice_value, self.max_dice_value + 1):
+            if self.values[str(i)] < 1:
+                return False
+
+        return True
+
     def score(self, dice_values: List[int]):
         if len(dice_values) != self.number_of_dices:
             raise ValueError("Number of dice values is different \
                              of the declared number of dices.")
 
-        self._reset_values()
+        self._fill_values(dice_values)
+
+        # Check if there is a kind
+        multiplicator = 2
+        for i in range(4, self.max_dice_value + 1):
+            if self._is_a_kind(i):
+                self.score_value *= multiplicator
+                return self.score_value
+            multiplicator *= 2
+
+        # Check if there is a straight
+        if self._is_straigth():
+            self.score_value += 1200
+            return self.score_value
+
+        # Check if there is three paires
+        if self._is_three_pairs():
+            self.score_value += 800
+            return self.score_value
+
+        # Check if there is a triple ones
+        if self._is_triple_with_value(1):
+            self.score_value += 1000
+
+        # Check if there is a triple
+        for i in range(2, self.max_dice_value):
+            if self._is_triple_with_value(i):
+                self.score_value += 100 * i
+
+        # Check if there is a single five
+        if self._is_single_five:
+            self.score_value += 50
+
+        # Check if there is a single one
+        if self._is_single_one:
+            self.score_value += 10
+
+        return self.score_value
--- test/greed.py
+++ test/greed.py
@@ -19,7 +19,7 @@ class GreedTest(unittest.TestCase):
         }
 
         greed = Greed()
-        self.assertEqual(greed.score, 0)
+        self.assertEqual(greed.score_value, 0)
         self.assertEqual(greed.number_of_dices, 6)
         self.assertEqual(greed.min_dice_value, 1)
         self.assertEqual(greed.max_dice_value, 6)
@@ -32,7 +32,7 @@ class GreedTest(unittest.TestCase):
         }
 
         greed = Greed(number_of_dices=5, min_dice_value=2, max_dice_value=4)
-        self.assertEqual(greed.score, 0)
+        self.assertEqual(greed.score_value, 0)
         self.assertEqual(greed.number_of_dices, 5)
         self.assertEqual(greed.min_dice_value, 2)
         self.assertEqual(greed.max_dice_value, 4)
@@ -77,6 +77,129 @@ class GreedTest(unittest.TestCase):
 
         self.assertEqual(greed.values, values)
 
+    def test_fill_values(self):
+        greed = Greed()
+        dice_values = [1, 2, 3, 2, 5, 4]
+        values = {
+            "1": 1,
+            "2": 2,
+            "3": 1,
+            "4": 1,
+            "5": 1,
+            "6": 0
+        }
+
+        greed._fill_values(dice_values)
+        self.assertEquals(greed.values, values)
+
+    def test_single_one(self):
+        greed = Greed()
+
+        dice_values = [1, 2, 3, 2, 5, 4]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_single_one())
+
+        dice_values = [1, 1, 3, 2, 5, 4]
+        greed._fill_values(dice_values)
+        self.assertFalse(greed._is_single_one())
+
+    def test_single_five(self):
+        greed = Greed()
+
+        dice_values = [1, 2, 3, 2, 5, 4]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_single_five())
+
+        dice_values = [1, 1, 3, 2, 5, 5]
+        greed._fill_values(dice_values)
+        self.assertFalse(greed._is_single_five())
+
+    def test_triple(self):
+        greed = Greed()
+
+        dice_values = [1, 2, 3, 2, 5, 4]
+        greed._fill_values(dice_values)
+        self.assertFalse(greed._is_triple_with_value(1))
+
+        dice_values = [1, 1, 1, 2, 5, 5]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_triple_with_value(1))
+
+        dice_values = [1, 1, 3, 2, 5, 1]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_triple_with_value(1))
+
+        dice_values = [1, 1, 3, 2, 1, 1]
+        greed._fill_values(dice_values)
+        self.assertFalse(greed._is_triple_with_value(1))
+
+    def test_kind(self):
+        greed = Greed()
+
+        dice_values = [1, 2, 3, 2, 5, 4]
+        greed._fill_values(dice_values)
+        self.assertFalse(greed._is_a_kind(4))
+
+        dice_values = [1, 2, 3, 2, 2, 4]
+        greed._fill_values(dice_values)
+        self.assertFalse(greed._is_a_kind(4))
+
+        dice_values = [2, 2, 3, 2, 2, 4]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_a_kind(4))
+
+        dice_values = [1, 1, 1, 2, 1, 1]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_a_kind(5))
+
+        dice_values = [1, 1, 1, 1, 1, 1]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_a_kind(6))
+
+    def test_three_pairs(self):
+        greed = Greed()
+
+        dice_values = [1, 2, 3, 2, 5, 4]
+        greed._fill_values(dice_values)
+        self.assertFalse(greed._is_three_pairs())
+
+        dice_values = [1, 1, 2, 2, 3, 3]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_three_pairs())
+
+        dice_values = [1, 1, 1, 1, 1, 1]
+        greed._fill_values(dice_values)
+        self.assertFalse(greed._is_three_pairs())
+
+        dice_values = [1, 2, 3, 3, 2, 1]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_three_pairs())
+
+        dice_values = [1, 2, 3, 1, 2, 3]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_three_pairs())
+
+    def test_straight(self):
+        greed = Greed()
+
+        dice_values = [1, 2, 3, 2, 5, 4]
+        greed._fill_values(dice_values)
+        self.assertFalse(greed._is_straigth())
+
+        dice_values = [1, 2, 3, 4, 5, 6]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_straigth())
+
+        dice_values = [4, 1, 6, 3, 2, 5]
+        greed._fill_values(dice_values)
+        self.assertTrue(greed._is_straigth())
+
+    def test_score(self):
+        greed = Greed()
+
+        dice_values = [1, 2, 3, 4, 5, 6]
+        score = greed.score(dice_values)
+        self.assertEquals(score, 1200)
 
 if __name__ == '__main__':
     unittest.main()
